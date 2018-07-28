54471d7e52655599b82fa6daa4858913f6cf064e
Louis Antoine Otter <louis.otter@gmail.com>
2018-07-28 02:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians, instruments){
+
+    let band = [];
+
+    for (let i = 0; i < musicians.length; i++){
+
+      band.push(`${musicians[i]} plays ${instruments[i]}`)
+
+
+
+    }
+
+    return band;
+
+}
+
+function johnLennonFacts (input){
+  let i = 0;
+  let output = [];
+
+  while (i < input.length){
+    output.push(`${input[i]}!!!`);
+    i++;
+  }
+  return output;
+}
